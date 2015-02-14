03670fb350e817c00866a87d51e7ede4ebb7acf5
Julian Duque <julianduquej@gmail.com>
2015-02-14 10:48:33 -0500

    improve and secure deploy process
    This will avoid DDoS on /update route adding proper github webhooks
    validation, better deploy solutions were suggested in #40 but this will
    suffice from now

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
+'use strict';
+
 var express = require('express'),
-    harp    = require('harp'),
-    path    = require('path'),
-    fs      = require('fs'),
+    webhook = require('github-webhook-middleware'),
     exec    = require('child_process').exec,
+    config  = require('./config'),
     events  = require('./events'),
     router  = require('./router'),
-    app     = express();
+    app     = express(),
+    port    = process.env.PORT || 8080;
+
+// Setup Github Webhooks
+var validateWebhook = webhook({
+  secret: config.github.secret
+});
 
 // Static Server
 app.use(express.static(__dirname + '/public'));
@@ -16,23 +23,32 @@ app.use(router);
 // Fetch Events
 app.get('/events.json', function (req, res) {
   events(function (err, data) {
-    if (err) {
+    if (err)
       return res.status(500).send(err.message);
-    }
 
     res.send(data);
   });
 });
 
 // Update site
-app.post('/update', function (req, res) {
-  exec('git pull && grunt build', function (err, stdout, stderr) {
-    if (err) return;
+app.post('/update', validateWebhook, function (req, res) {
+  if (req.headers['x-github-event'] !== 'push')
+      return res.status(200).json({ status: 'nothing to do' });
 
+  exec('git pull && grunt build', function (err, stdout, stderr) {
+    if (err) return res.status(500).send(err.message);
+
+    // Log git and grunt output
     console.log(stdout);
-    throw new Error('Restart action required');
+    console.error(stderr);
+
+    res.status(200).json({ status: 'ok' });
+
+    // Restart node process after update
+    process.exit(0);
   });
 });
 
-app.listen(8080);
-console.log("MedellinJS started");
+app.listen(port, function () {
+  console.log('MedellinJS listening on http://localhost:%s', port);
+});
