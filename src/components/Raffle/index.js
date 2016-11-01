69e02493e412eab25e94729477cd77a5190df9ba
Fernando Montoya <montogeek@gmail.com>
2016-11-01 15:31:43 -0500

    Raffle: No duplicates and better error handling

--- src/components/Raffle/index.js
+++ src/components/Raffle/index.js
@@ -1,19 +1,43 @@
 import React from 'react';
 import jsonp from 'jsonp';
 
+
+const ShowWinner = ({winner}) => (
+  <p>{winner}</p>
+)
+
+const ShowError = ({error}) => (
+  <div>
+    {error}
+  </div>
+)
+
+const ShowButton = ({onClick}) => (
+  <button onClick={onClick}>{"Get a Winner!"}</button>
+)
+
 export default class Raffle extends React.Component {
   constructor(props) {
     super(props);
 
+    this.handleClick = this.handleClick.bind(this);
     this.state = {
       attendees: [],
       loaded: false,
-      error: false
+      error: false,
+      winner: ""
     }
   }
 
-  renderWinner() {
-    return this.state.attendees[Math.floor(Math.random() * (this.state.attendees.length))];
+  handleClick() {
+    const { attendees } = this.state;
+    const index = Math.floor(Math.random() * attendees.length);
+    const winner = attendees.splice(index, 1)[0];
+
+    this.setState({
+      attendees,
+      winner
+    });
   }
 
   componentDidMount() {
@@ -21,30 +45,42 @@ export default class Raffle extends React.Component {
       if(error) {
         this.setState({ error });
       } else {
-        const { id } = response.data[0];
-        jsonp(`https://api.meetup.com/medellinjs/events/${id}/rsvps?photo-host=public&sig_id=157972512&response=yes&sig=035ca860db7018da8e1a7366578ac5bcb1e02627`, null, (error, response) => {
-          if(error) {
-            this.setState({ error });
-          } else {
-            const attendees = response.data.map((attendee) => attendee.member.name);
+        if(response.data.length !== 0) {
+          const { id } = response.data[0];
+          jsonp(`https://api.meetup.com/medellinjs/events/${id}/rsvps?photo-host=public&sig_id=157972512&response=yes&sig=035ca860db7018da8e1a7366578ac5bcb1e02627`, null, (error, response) => {
+            if(error) {
+              this.setState({ error });
+            } else {
+              const attendees = response.data.map((attendee) => attendee.member.name);
 
-            this.setState({ attendees, loaded: true })
-          }
+              this.setState({ attendees, loaded: true })
+            }
+          })
+        }
+        this.setState({
+          error: 'No hay evento :(',
+          loaded: true
         })
       }
     })
+  }
+
+  showRaffle() {
+    const { winner, error } = this.state;
+    return (
+      <div>
+        {!error && <ShowButton onClick={this.handleClick} /> }
+        {winner ? <ShowWinner winner={winner} /> : ''}
+      </div>
+    )
   }
 
   render() {
     const { loaded, error } = this.state;
     return (
       <div>
-      {loaded &&
-        this.renderWinner()
-      }
-      {error &&
-        error
-      }
+        {loaded ? this.showRaffle() : 'Cargando...'}
+        {error && <ShowError error={error} />}
       </div>
     );
   }
