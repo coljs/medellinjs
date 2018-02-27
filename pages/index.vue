f7326469f448bea3b2c4bff575b3da05fe2c8920
Jesse Cogollo <jesse@adviceinteractive.com>
2018-02-27 01:04:56 -0500

    implementation communities setcion #114

--- pages/index.vue
+++ pages/index.vue
@@ -28,45 +28,8 @@
         </div>
       </div>
 
-      <!--------------- Intro Area -------------->
-      <div id="intro-area" class="section section-padding-extra">
-        <div class="container">
-          <div class="row">
-            <div class="intros">
-              <div class="col-sm-4">
-                <div class="intro text-center wow animated fadeInUp" data-wow-duration="1.5s">
-                  <span class="intro-icon">
-                    <i class="fa fa-paint-brush"></i>
-                  </span>
-                  <h4 class="intro-heading">Unique Design</h4>
-                  <p>Lorem ipsum dolor sit amet, consectetured adipiscing elit, sed do eiusmod tempor inci didunt ut labore
-                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exer citation ullamco laboris.</p>
-                </div>
-              </div>
-              <div class="col-sm-4">
-                <div class="intro text-center wow animated fadeInUp" data-wow-delay="0.5s" data-wow-duration="1.5s">
-                  <span class="intro-icon">
-                    <i class="fa fa-angellist"></i>
-                  </span>
-                  <h4 class="intro-heading">Corporate Branding</h4>
-                  <p>Lorem ipsum dolor sit amet, consectetured adipiscing elit, sed do eiusmod tempor inci didunt ut labore
-                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exer citation ullamco laboris.</p>
-                </div>
-              </div>
-              <div class="col-sm-4">
-                <div class="intro text-center wow animated fadeInUp" data-wow-delay="1s" data-wow-duration="1.5s">
-                  <span class="intro-icon">
-                    <i class="fa fa-support"></i>
-                  </span>
-                  <h4 class="intro-heading">Any time support</h4>
-                  <p>Lorem ipsum dolor sit amet, consectetured adipiscing elit, sed do eiusmod tempor inci didunt ut labore
-                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exer citation ullamco laboris.</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <!--------------- Communities Area -------------->
+      <AppCommunities />
 
       <!--------------- Freatures ------------------>
       <div id="features-area" class="section section-padding section-bg features-area">
@@ -820,6 +783,7 @@
 
 
 <script>
+import AppCommunities from '~/components/AppCommunities.vue'
 import AppHeader from '~/components/AppHeader.vue'
 import AppOrganizer from '~/components/AppOrganizer.vue'
 import AppNavBar from '~/components/AppNavBar.vue'
@@ -828,6 +792,7 @@ import AppFooter from '~/components/AppFooter.vue'
 
 export default {
   components: {
+    AppCommunities,
     AppHeader,
     AppOrganizer,
     AppNavBar,
