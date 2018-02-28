d0da5a4f221a62e787a1f5e29e687f9d45a762f6
Jesse Cogollo <jesse@adviceinteractive.com>
2018-02-27 22:09:15 -0500

    complete list colombian js communities.

--- components/AppCommunities.vue
+++ components/AppCommunities.vue
@@ -102,11 +102,38 @@ export default {
         popayanjs: {
           name: 'PopayánJS',
           bio: 'Comunidad JS Popayan',
-          twitter: 'khriztianmoreno',
-          github: 'khriztianmoreno',
+          twitter: '@popayanjs',
+          github: 'popayanjs',
           avatar:
             'http://i.imgur.com/CSM3nCr.png',
           website: 'http://www.meetup.com/PopayanJS/'
+        },
+        manizalesjs: {
+          name: 'manizalesJS',
+          bio: 'Comunidad JS Manizales',
+          twitter: '@manizalesjs',
+          github: 'manizalesjs',
+          avatar:
+            'https://secure.meetupstatic.com/photos/event/d/f/b/c/highres_433977276.jpeg',
+          website: 'https://www.meetup.com/ManizalesJS/'
+        },
+        barranquillajs: {
+          name: 'BarranquillaJS',
+          bio: 'Comunidad JS Barranquilla',
+          twitter: '@barranquillajs',
+          github: 'barranquillajs',
+          avatar:
+            'https://secure.meetupstatic.com/photos/member/5/8/5/c/highres_259462620.jpeg',
+          website: 'https://www.meetup.com/BarranquillaJS/'
+        },
+        bucaramangajs: {
+          name: 'BucaramangaJS',
+          bio: 'Comunidad JS Bucaramanga',
+          twitter: '@bucaramangajs',
+          github: 'bucaramangajs',
+          avatar:
+            'https://secure.meetupstatic.com/photos/event/a/9/1/2/highres_444343282.jpeg',
+          website: 'https://www.meetup.com/BucaramangaJS/'
         }
       }
     }
