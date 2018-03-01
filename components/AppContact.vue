49f62f33926b4fc0f901669954b8bb5a8ab8cece
Jesse Cogollo <jesse@adviceinteractive.com>
2018-02-28 22:52:53 -0500

    speaker form section

--- components/AppContact.vue
+++ components/AppContact.vue
@@ -2,39 +2,29 @@
   <div id="contact-area" class="section section-padding-extra contact-area">
     <div class="container">
       <div class="row">
-        <div class="col-md-6 col-md-offset-3 text-center">
+        <div class="col-md-12 text-center">
           <div class="section-heading">
-            <h2 class="section-title">Leave a message</h2>
-            <p class="section-subtitle">There are many variations of passages of Lorem Ipsum available</p>
+            <h2 class="section-title">Imparte una charla en MedellínJS</h2>
+            <p class="section-subtitle">Somos una comunidad abierta, por lo tanto brindamos el espacio ideal para que compartas tu conocimiento con los asistentes a nuestras reuniones.</p>
+            <p class="section-subtitle">Cada mes contamos con que dos miembros de la comunidad se animen a exponer sus experiencias en algún tema relacionado con JavaScript y así difundir el conocimiento personal para transformarlo en conocimiento colectivo. </p>
+            <p>Consejos generales para preparar tu charla</p>
+            <p class="section-subtitle">
+             <ul>
+              <li>Construye una presentación breve sobre el tema seleccionado</li>
+              <li>Crea ejemplos básicos sobre la herramienta o el tema de la charla</li>
+              <li>En lo posible crea un artículo sobre el tema para ser publicado luego en nuestro blog</li>
+              <li>Prepara un repositorio en Github para compartir los ejemplos con la comunidad</li>
+              <li>Compila una lista de referencias para que los asistentes puedan profundizar en el tema</li>
+            </ul>
+          </p>
           </div>
         </div>
       </div>
       <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-          <form id="contactForm" class="contact-form wow animated fadeInUp" data-wow-duration="1.5s" action="#" method="post">
-            <div class="row">
-              <div class="col-sm-6">
-                <input id="name" type="text" name="name" placeholder="Name" required>
-              </div>
-              <div class="col-sm-6">
-                <input id="email" type="email" name="email" placeholder="Email" required>
-              </div>
-              <div class="col-sm-12">
-                <input id="subject" type="text" name="subject" placeholder="Subject" required>
-              </div>
-              <div class="col-sm-12">
-                <textarea id="message" placeholder="Message" rows="4" required></textarea>
-              </div>
-              <div class="col-sm-12 text-center">
-                <button class="btn" type="submit">Submit Message</button>
-              </div>
-              <div class="col-md-12 text-center">
-                <p class="input-success">We have received your mail, We will get back to you soon!</p>
-                <p class="input-error">Sorry, Message could not send! Please try again.</p>
-              </div>
-            </div>
-          </form>
-        </div>
+          <div class="col-md-12 text-center">
+            <p>&nbsp;</p>
+            <iframe src="https://docs.google.com/spreadsheet/viewform?formkey=dHpQXzBrSllnQjd0RFZleExEenVrOVE6MQ#gid=0" style="width: 90%; height:1200px; border:none; overflow:hidden; min-width:660px;"></iframe>
+          </div>
       </div>
       <div class="row">
         <div class="contact-infos">
@@ -43,26 +33,19 @@
               <span class="contact-icon">
                 <i class="fa fa-map-marker"></i>
               </span>
-              <p class="contact-detail">68 Dohava Strees, Lorem isput Spusts
-                <br/>New York, United State</p>
+              <p class="contact-detail">Complejo Ruta N Calle 67 Nº 52-20
+                <br/>Medellín - Colombia</p>
             </div>
+          </div>
+          <div class="col-sm-4 text-center">
           </div>
           <div class="col-sm-4 text-center">
             <div class="contact-item wow animated fadeInUp" data-wow-delay="0.5s" data-wow-duration="1.5s">
               <span class="contact-icon">
                 <i class="fa fa-envelope"></i>
               </span>
-              <p class="contact-detail">info@geo.com
-                <br/>support@geo.com</p>
-            </div>
-          </div>
-          <div class="col-sm-4 text-center">
-            <div class="contact-item wow animated fadeInUp" data-wow-delay="1s" data-wow-duration="1.5s">
-              <span class="contact-icon">
-                <i class="fa fa-phone"></i>
-              </span>
-              <p class="contact-detail">+123-456-7890 - Central Office
-                <br/>+123-456-7890 - Fax</p>
+              <p class="contact-detail">medellinjs@gmail.com
+                <br/>https:// github.com/coljs/medellinjs</p>
             </div>
           </div>
         </div>
