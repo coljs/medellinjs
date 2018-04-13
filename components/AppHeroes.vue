afc9f7877490d4f18a942d2121b3d993b8e34642
khriztianmoreno <khriztianmoreno@gmail.com>
2018-04-13 08:14:10 -0500

    Delete the anchor to open a modal

--- components/AppHeroes.vue
+++ components/AppHeroes.vue
@@ -12,11 +12,11 @@
       <div class="row">
         <div class="col-md-10 col-md-offset-1">
           <div id="heroes" class="heroes">
-            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profileHero" v-for="hero in heroes">
-              <div class="img-box reviewer-mock" v-b-modal="'modal-' + hero.username">
-                <img v-bind:src="hero.avatar" class="img-responsive">
+            <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12 profileHero" v-for="(hero, key) in heroes" :key="key">
+              <div class="img-box reviewer-mock">
+                <img :src="hero.avatar" class="img-responsive">
                 <ul class="text-center hidden-sm hidden-xs">
-                   <a target="_blank" v-bind:href="hero.github"><i class="fa fa-github"></i></a>
+                   <a target="_blank" :href="hero.github"><i class="fa fa-github"></i></a>
                    <a target="_blank" :href="hero.twitter"><i class="fa fa-twitter"></i></a>
                    <a v-b-modal="'modal-' + hero.username"><i class="fa fa-comment"></i></a>
                 </ul>
@@ -24,25 +24,11 @@
               <div class="text-center ">
                 <h1>{{hero.name}}</h1>
                 <div class="member-connections hidden-lg hidden-md">
-                  <a target="_blank" v-bind:href="hero.github"><i class="fa fa-github"></i></a>
+                  <a target="_blank" :href="hero.github"><i class="fa fa-github"></i></a>
                   <a target="_blank" :href="hero.twitter"><i class="fa fa-twitter"></i></a>
                   <a v-b-modal="'modal-' + hero.username"><i class="fa fa-comment"></i></a>
                 </div>
               </div>
-
-              <b-modal size="lg" hide-footer v-bind:id="'modal-' +hero.username" :title="'Charlas de '+hero.name">
-                <ul class="timeline">
-                  <li v-for="slide in hero.slides">
-                    <div :class="'direction-'+slide.direction">
-                      <div class="flag-wrapper">
-                        <span class="flag">{{slide.title}}</span>
-                        <!-- <span class="time-wrapper"><span class="time">fecha</span></span> -->
-                      </div>
-                      <div class="desc"><a :href="slide.url">{{slide.url}}</a></div>
-                    </div>
-                  </li>
-                </ul>
-              </b-modal>
             </div>
           </div>
         </div>
