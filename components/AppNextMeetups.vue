eff36320252a38e219560003b8319b9eb496947e
Ely Alvarado <elyalvarado@gmail.com>
2018-04-19 18:01:38 -0500

    Include at least the last past meetup in the list of meetups

--- components/AppNextMeetups.vue
+++ components/AppNextMeetups.vue
@@ -12,18 +12,31 @@
       <div class="row">
         <div class="price-tables">
 
-          <div v-if="loading" class="col-sm-4 col-sm-offset-4">
+          <div v-if="!finishedLoading(loaded)" class="col-sm-4 col-sm-offset-4">
             <div class="price-table text-center wow active animated fadeInUp" data-wow-delay="0" data-wow-duration="1.5s">
               <div class="price-heading">
-                <h2 class="price-plan">Loading</h2>
+                <h2 class="price-plan">Cargando</h2>
               </div>
             </div>
           </div>
 
           <template v-else >
-            <div class="col-sm-4" v-for="(meetup, index) in nextMeetups" :key="index">
+            <div class="col-sm-4" v-for="(meetup, index) in pastMeetups" :key="meetup.id">
               <a :href="meetup.link" target="new">
-                <div class="price-table text-center wow animated fadeInUp" :data-wow-delay="0.5*index+'s'" data-wow-duration="1.5s">
+                <div class="price-table text-center active wow animated fadeInUp" :data-wow-delay="0.5*index+'s'" data-wow-duration="1.5s">
+                  <div class="price-heading">
+                    <div class="price-plan">{{meetup.name}}</div>
+                    <h3 class="plan-price">
+                      <small class="price-cycle">{{formatDate(meetup.time)}} <span class="label label-danger">pasó</span></small>
+                    </h3>
+                  </div>
+                </div>
+              </a>
+            </div>
+
+            <div class="col-sm-4" v-for="(meetup, index) in nextMeetups" :key="meetup.id">
+              <a :href="meetup.link" target="new">
+                <div class="price-table text-center wow animated fadeInUp" :data-wow-delay="(0.5*pastMeetups.length+0.5*index)+'s'" data-wow-duration="1.5s">
                   <div class="price-heading">
                     <h2 class="price-plan">{{meetup.name}}</h2>
                     <h3 class="plan-price">
@@ -35,7 +48,7 @@
             </div>
           </template>
 
-          <div class="col-sm-8 col-sm-offset-2" v-if="!nextMeetups.length && !loading" >
+          <div class="col-sm-8 col-sm-offset-2" v-if="!nextMeetups.length && !pastMeetups.length && finishedLoading(loaded)" >
             <div class="price-table text-center wow animated fadeInUp" :data-wow-delay="0" data-wow-duration="1.5s">
               <div class="price-heading">
                 <h3 class="plan-price">
@@ -62,28 +75,50 @@ const formatDate = (d) => {
   return dateObject.toLocaleDateString(locale, options)
 }
 
+const finishedLoading = (loaded) => {
+  return Object.keys(loaded).reduce((acc, loader) => loaded[loader] && acc, true)
+}
+
 export default {
   data () {
     return {
-      loading: true,
-      nextMeetups: []
+      nextMeetups: [],
+      pastMeetups: [],
+      loaded: {
+        pastEvents: false,
+        nextEvents: false
+      }
     }
   },
   methods: {
-    formatDate
+    formatDate,
+    finishedLoading
   },
   mounted () {
     let self = this
     const nextMeetupsURL = 'https://api.meetup.com/MedellinJS/events?photo-host=public&page=20&sig_id=233024411&sig=4f25bc7667fd757ce3cda2aa99feacf3b8ce5fbd'
+    const pastMeetupsURL = 'https://api.meetup.com/MedellinJS/events?desc=true&photo-host=public&page=1&sig_id=233024411&status=past&sig=032b49e98bab5fc4368836d6f71e5b2c00fb3154'
+
     jsonp(nextMeetupsURL, {}, function (err, data) {
       if (err) {
         console.error('Error fetching next meetups from MeetupAPI')
-        self.loading = false
+        self.loaded.nextEvents = true
         return
       }
 
-      self.loading = false
+      self.loaded.nextEvents = true
       self.nextMeetups = data.data
+    })
+
+    jsonp(pastMeetupsURL, {}, function (err, data) {
+      if (err) {
+        console.error('Error fetching past meetups from MeetupAPI')
+        self.loaded.pastEvents = true
+        return
+      }
+
+      self.loaded.pastEvents = true
+      self.pastMeetups = data.data
     })
   }
 }
