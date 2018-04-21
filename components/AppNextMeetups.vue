770cf6d2acc6d55c8cb1a434f030d976c418f3db
Ely Alvarado <elyalvarado@gmail.com>
2018-04-21 13:49:02 -0500

    Added custom semantic styles for the events area

--- components/AppNextMeetups.vue
+++ components/AppNextMeetups.vue
@@ -1,3 +1,125 @@
+<style scoped>
+.meetup-tables {
+  float: left;
+  width: 100%;
+  margin-bottom: -30px;
+}
+
+.meetup-table {
+  float: left;
+  width: 100%;
+  margin-bottom: 30px;
+  position: relative;
+}
+
+.meetup-heading {
+  background: #fff;
+  border-radius: 4px;
+  transition: all 0.3s ease;
+}
+
+.meetup-table {
+  &:hover .meetup-heading, &.past .meetup-heading {
+    background-color: #1293d5;
+  }
+  &:hover .meetup-event, &:hover .meetup-cycle, &.past .meetup-event, &.past .meetup-cycle {
+    color: #fff;
+  }
+}
+
+.meetup-event {
+  font-weight: 800;
+  color: #1293d5;
+  background-color: rgba(0, 0, 0, 0.11);
+  margin: 0;
+  padding: 33px 20px;
+}
+
+h2.meetup-event {
+  text-transform: none;
+  font-size: 2rem;
+  line-height: 1.2;
+}
+
+.event-meetup {
+  font-size: 1.2rem;
+  font-family: 'Roboto', sans-serif;
+  color: #1293d5;
+  margin: 0;
+  padding: 19px 20px;
+}
+
+.meetup-cycle {
+  display: inline-block;
+  vertical-align: baseline;
+  font-size: 1.5rem;
+  font-weight: 400;
+  text-transform: lowercase;
+}
+
+.ribbon-wrapper {
+  overflow: hidden;
+  position: absolute;
+  top: -2px;
+  left: -2px;
+  bottom: -2px;
+  right: -2px;
+}
+
+.ribbon-wrapper .ribbon {
+  position: absolute;
+  padding: 0px 0;
+  font-weight: bold;
+  font-size: 1.1rem;
+  color: #333;
+  text-align: center;
+  background-color: #ebb134;
+  color: #fff;
+  width: 100px;
+}
+
+.ribbon.top-left{
+  -webkit-transform: rotate(-45deg);
+  -moz-transform:    rotate(-45deg);
+  -ms-transform:     rotate(-45deg);
+  -o-transform:      rotate(-45deg);
+  top: 15px;
+  left: -25px;
+}
+
+.ribbon.top-right{
+  top: 15px;
+  right: -25px;
+  transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
+}
+
+.ribbon.bottom-left{
+  bottom: 15px;
+  left: -25px;
+  transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
+}
+
+.ribbon.bottom-right{
+  right: -25px;
+  bottom: 15px;
+  transform: rotate(-45deg);
+  -webkit-transform: rotate(-45deg);
+}
+
+.ribbon.white{background: #f0f0f0; color: #555;}
+.ribbon.black{background: #333;}
+.ribbon.grey{background: #999;}
+.ribbon.blue{background: #39d;}
+.ribbon.green{background: #2c7;}
+.ribbon.turquoise{background: #1b9;}
+.ribbon.purple{background: #95b;}
+.ribbon.red{background: #e43;}
+.ribbon.orange{background: #e82;}
+.ribbon.yellow{background: #ec0;}
+</style>
+
 <template>
   <div id="next-meetups-area" class="section section-padding-extra section-bg">
     <div class="container">
@@ -10,12 +132,12 @@
         </div>
       </div>
       <div class="row">
-        <div class="price-tables">
+        <div class="meetup-tables">
 
           <div v-if="!finishedLoading(loaded)" class="col-sm-4 col-sm-offset-4">
-            <div class="price-table text-center wow active animated fadeInUp" data-wow-delay="0" data-wow-duration="1.5s">
-              <div class="price-heading">
-                <h2 class="price-plan">Cargando</h2>
+            <div class="meetup-table text-center wow past animated fadeInUp" data-wow-delay="0" data-wow-duration="1.5s">
+              <div class="meetup-heading">
+                <h1 class="meetup-event">Cargando</h1>
               </div>
             </div>
           </div>
@@ -23,24 +145,25 @@
           <template v-else >
             <div class="col-sm-4" v-for="(meetup, index) in pastMeetups" :key="meetup.id">
               <a :href="meetup.link" target="new">
-                <div class="price-table text-center active wow animated fadeInUp" :data-wow-delay="0.5*index+'s'" data-wow-duration="1.5s">
-                  <div class="price-heading">
-                    <div class="price-plan">{{meetup.name}}</div>
-                    <h3 class="plan-price">
-                      <small class="price-cycle">{{formatDate(meetup.time)}} <span class="label label-danger">pasó</span></small>
+                <div class="meetup-table text-center past wow animated fadeInUp" :data-wow-delay="0.5*index+'s'" data-wow-duration="1.5s">
+                  <div class="meetup-heading">
+                    <h2 class="meetup-event">{{meetup.name}}</h2>
+                    <h3 class="event-meetup">
+                      <div class="meetup-cycle">{{formatDate(meetup.time)}}</div>
                     </h3>
                   </div>
+                  <div class="ribbon-wrapper"><div class="ribbon bottom-right red">pasó</div></div>
                 </div>
               </a>
             </div>
 
             <div class="col-sm-4" v-for="(meetup, index) in nextMeetups" :key="meetup.id">
               <a :href="meetup.link" target="new">
-                <div class="price-table text-center wow animated fadeInUp" :data-wow-delay="(0.5*pastMeetups.length+0.5*index)+'s'" data-wow-duration="1.5s">
-                  <div class="price-heading">
-                    <h2 class="price-plan">{{meetup.name}}</h2>
-                    <h3 class="plan-price">
-                      <small class="price-cycle">{{formatDate(meetup.time)}}</small>
+                <div class="meetup-table text-center wow animated fadeInUp" :data-wow-delay="(0.5*pastMeetups.length+0.5*index)+'s'" data-wow-duration="1.5s">
+                  <div class="meetup-heading">
+                    <h2 class="meetup-event">{{meetup.name}}</h2>
+                    <h3 class="event-meetup">
+                      <div class="meetup-cycle">{{formatDate(meetup.time)}}</div>
                     </h3>
                   </div>
                 </div>
@@ -49,10 +172,10 @@
           </template>
 
           <div class="col-sm-8 col-sm-offset-2" v-if="!nextMeetups.length && !pastMeetups.length && finishedLoading(loaded)" >
-            <div class="price-table text-center wow animated fadeInUp" :data-wow-delay="0" data-wow-duration="1.5s">
-              <div class="price-heading">
-                <h3 class="plan-price">
-                  <small class="price-cycle">No hay eventos programados</small>
+            <div class="meetup-table text-center wow animated fadeInUp" :data-wow-delay="0" data-wow-duration="1.5s">
+              <div class="meetup-heading">
+                <h3 class="event-meetup">
+                  <small class="meetup-cycle">No hay eventos programados</small>
                 </h3>
               </div>
             </div>
