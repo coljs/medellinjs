9b37076fcd232c094f56c12d2592a67c2776e8dc
brandonvg89 <brandonvg89@gmail.com>
2018-04-25 19:59:47 -0500

    Migrar data comunidades a la carpeta assets

--- components/AppCommunities.vue
+++ components/AppCommunities.vue
@@ -37,105 +37,11 @@
 </template>
 
 <script>
+import communities from '@/assets/data/communities.js'
 export default {
   data () {
     return {
-      communities: {
-        bogotajs: {
-          name: 'BogotáJS',
-          bio:
-            'Comunidad JS Bogotá',
-          twitter: '@bogotajs',
-          github: 'bogotajs',
-          avatar:
-            'http://imgur.com/0VE7Df0.png',
-          website: 'http://www.bogotajs.com/'
-        },
-        calijs: {
-          name: 'CaliJS',
-          bio:
-            'Comunidad JS Cali',
-          twitter: '@calijs',
-          github: 'calijs',
-          avatar:
-            'https://i.imgur.com/sBZF10C.png',
-          website: 'http://www.meetup.com/CaliJS/'
-        },
-        villavicenciojs: {
-          name: 'VillavicencioJS',
-          bio:
-            'Comunidad JS Villavicencio',
-          twitter: '@villavicenciojs',
-          github: 'villavicenciojs',
-          avatar:
-            'https://i.imgur.com/6RKQMj1.png',
-          website: 'http://www.meetup.com/es-ES/Villavicencio-JS/'
-        },
-        quibdojs: {
-          name: 'QuibdóJS',
-          bio:
-            'Comunidad JS Quibdó',
-          twitter: '@quibdojs',
-          github: 'quibdojs',
-          avatar:
-            'http://i.imgur.com/MQwQ9Kt.png',
-          website: 'https://twitter.com/QuibdoJs'
-        },
-        pereirajs: {
-          name: 'PereiraJS',
-          bio: 'Comunidad JS Pereira',
-          twitter: '@pereirajs',
-          github: 'pereirajs',
-          avatar:
-            'https://i.imgur.com/K74Qa1f.png',
-          website: 'http://www.pereirajs.org/'
-        },
-        monteriajs: {
-          name: 'MonteríaJS',
-          bio: 'Comunidad JS Monteria',
-          twitter: '@monteriajs',
-          github: 'monteriajs',
-          avatar:
-            'http://i.imgur.com/e35RPp3.png',
-          website: 'http://www.meetup.com/monteriajs/'
-        },
-        popayanjs: {
-          name: 'PopayánJS',
-          bio: 'Comunidad JS Popayan',
-          twitter: '@popayanjs',
-          github: 'popayanjs',
-          avatar:
-            'http://i.imgur.com/CSM3nCr.png',
-          website: 'http://www.meetup.com/PopayanJS/'
-        },
-        manizalesjs: {
-          name: 'manizalesJS',
-          bio: 'Comunidad JS Manizales',
-          twitter: '@manizalesjs',
-          github: 'manizalesjs',
-          avatar:
-            'https://secure.meetupstatic.com/photos/event/d/f/b/c/highres_433977276.jpeg',
-          website: 'https://www.meetup.com/ManizalesJS/'
-        },
-        barranquillajs: {
-          name: 'BarranquillaJS',
-          bio: 'Comunidad JS Barranquilla',
-          twitter: '@barranquillajs',
-          github: 'barranquillajs',
-          avatar:
-            'https://secure.meetupstatic.com/photos/member/5/8/5/c/highres_259462620.jpeg',
-          website: 'https://www.meetup.com/BarranquillaJS/'
-        },
-        bucaramangajs: {
-          name: 'BucaramangaJS',
-          bio: 'Comunidad JS Bucaramanga',
-          twitter: '@bucaramangajs',
-          github: 'bucaramangajs',
-          avatar:
-            'https://secure.meetupstatic.com/photos/event/a/9/1/2/highres_444343282.jpeg',
-          website: 'https://www.meetup.com/BucaramangaJS/'
-        }
-      }
+      communities
     }
   }
 }
