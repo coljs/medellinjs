12274ae864b4f47ef44b34534215bcc2c63e9af3
Pablo Quiceno <jpquiceno@kometsales.com>
2018-04-25 20:19:56 -0500

    Se agregan link de quieres ser sponsor con el pdf de prospecto

---
+++ pages/sponsor/index.vue
@@ -0,0 +1,10 @@
+<template>
+</template>
+
+<script>
+export default {
+  mounted () {
+    window.location.replace('https://drive.google.com/file/d/1AAFjSuUHwXvk_ObmMzIB98yRzPvMsLlN/view')
+  }
+}
+</script>
