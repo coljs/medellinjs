a1e334bee665c4f32ec4561725b04ad3a89648b4
Maria Camila Gómez <camigomez@experimentality.co>
2018-05-13 00:42:56 -0500

    Show attendence of meetup selected and add styles

--- pages/sorteo/index.vue
+++ pages/sorteo/index.vue
@@ -9,18 +9,41 @@
     </header><!-- /header -->
     <div id="main">
       <section>
-          <div class="container">
+        <div class="container">
+          <div>
+            <div class="row">
+              <div class="col-md-2 text-right">
+                <label id="label-meetup" for="meetup-id">Meetup ID:</label>
+              </div>
+              <div class="col-md-3">
+                <input id="meetup-id" type="text" v-model="meetupID">
+              </div>
+              <div class="col-md-6">
+                <button class="button" v-on:click="searchAttendence()">Cargar asistentes</button>
+                <button class="button" v-on:click="searchWinner()">Ganador</button>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <select class="col-md-6" disabled size="13" v-if="show.attendance">
+              <option class="animated slideInUp" v-for="person in attendance" :key="person.member.id">{{person.member.name}}</option>
+            </select>
+
+            <div class="container animated zoomInDown col-md-6" v-if="show.winner">
               <h1>Ganador:</h1>
               <div id="info-winner">
-                <div class="floating-box">
+                <div>
                   <img :src="ganador.photo">
                 </div>
-                <div class="floating-box">
+                <div>
                   <div id="name">{{ganador.name}}</div>
                   <div id="id">{{ganador.id}}</div>
                 </div>
               </div>
+            </div>
           </div>
+
+        </div>
       </section>
     </div><!-- #main -->
     <app-footer />
@@ -30,33 +53,45 @@
 <script>
   import AppFooter from '~/components/AppFooter'
   import jsonp from 'jsonp'
+
   export default {
     components: {
       AppFooter: AppFooter
     },
     data () {
       return {
+        ganador: {},
         attendance: [],
-        ganador: {}
+        meetupID: '249414634',
+        show: {
+          attendance: false,
+          winner: false
+        }
       }
     },
-    mounted () {
-      let self = this
-      const meetup = 'https://api.meetup.com/MedellinJS/events/249414634/attendance'
-
-      jsonp(meetup, {}, function (err, data) {
-        if (err) {
-          console.error('Error fetching next meetups from MeetupAPI')
-          return
-        }
-        let attendance = data.data
-        let ganadorIndex = Math.floor(Math.random() * (attendance.length + 1))
-        let ganadorInfo = attendance[ganadorIndex].member
+    methods: {
+      searchAttendence: function () {
+        let self = this
+        const meetup = 'https://api.meetup.com/MedellinJS/events/' + self.meetupID + '/attendance'
+        jsonp(meetup, {}, function (err, data) {
+          if (err) {
+            console.error('Error fetching next meetups from MeetupAPI')
+            self.attendance = []
+            self.show.attendance = false
+            return
+          }
+          self.show.attendance = true
+          self.attendance = data.data
+        })
+      },
+      searchWinner: function () {
+        let self = this
+        let ganadorIndex = Math.floor(Math.random() * (self.attendance.length + 1))
+        let ganadorInfo = self.attendance[ganadorIndex].member
         let ganador = { id: ganadorInfo.id, name: ganadorInfo.name, photo: ganadorInfo.photo.photo_link }
-
-        self.attendance = data.data
         self.ganador = ganador
-      })
+        self.show.winner = true
+      }
     }
   }
 </script>
@@ -84,10 +119,6 @@
     border-radius: 50%;
   }
 
-  section div#info-winner .floating-box {
-    display: inline-block;
-    margin: 10px;
-  }
   section div#name {
     top: 30px;
     font-size: 50px;
@@ -118,4 +149,36 @@
     color: white;
     font-weight: 400;
   }
+
+  button {
+    background-color: #1193d4;
+    color: white;
+    padding: 12px 20px;
+    margin-right: 10px;
+    border-radius: 8px;
+    font-family: Raleway,sans-serif;
+    font-weight: 400;
+    text-transform: capitalize;
+    border-radius: 4px;
+  }
+
+  button:hover, button:active {
+    background-color: #0683c9;
+  }
+
+  select {
+    border: 0;
+    text-align: center;
+    font-size: 30px;
+    font-family: Raleway,sans-serif;
+  }
+  label#label-meetup {
+    margin-top: 10px;
+  }
+  input#meetup-id{
+    border-radius: 8px;
+    margin-top: 5px;
+    border: 1px solid #1193d4;
+  }
+
 </style>
