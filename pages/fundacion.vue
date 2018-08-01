30b2ead2e40a147afb358946ceefbf32b927400a
Jesse Cogollo <jesse@adviceinteractive.com>
2018-08-01 18:23:49 -0500

    task #213

---
+++ pages/fundacion.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <!--------------- Main Body Area -------------->
+    <div class="main-wrap">
+        
+      <!--------------- Fundacion -------------->
+      <AppFundacion />
+
+    </div>
+    <!--------------- Footer -------------->
+    <app-footer />
+
+    <a id="to-top" class="to-top" href="#top-header" data-spy="affix" data-offset-top="500">
+      <i class="fa fa-chevron-up"></i>
+    </a>
+
+  </div>
+</template>
+
+<script>
+import AppPreLoader from '~/components/AppPreLoader.vue'
+import AppFundacion from '~/components/fundation/index.vue'
+import AppFooter from '~/components/AppFooter.vue'
+
+export default {
+  components: {
+    AppPreLoader,
+    AppFundacion,
+    AppFooter
+  }
+}
+</script>
