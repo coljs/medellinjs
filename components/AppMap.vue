3e7893311703bcea32feb4ffdf1c7ee3fcfe70ce
Jesse cogollo <cogollo87@gmail.com>
2021-04-19 13:46:25 -0500

    husky auto changes

--- components/AppMap.vue
+++ components/AppMap.vue
@@ -6,7 +6,7 @@
       data-lat="23.75043"
       data-lng="90.38844"
       data-zoom="16"
-      style="height: 330px;"
+      style="height: 330px"
     ></div>
   </div>
 </template>
--- pages/sponsor/index.vue
+++ pages/sponsor/index.vue
@@ -1,7 +1,7 @@
 <template>
   <iframe
     src="//drive.google.com/file/d/1qIKIMFn0B2ekJSTMMkUkpJcSvkbF_zu2/preview"
-    style="width: 100%; height: 980px;"
+    style="width: 100%; height: 980px"
     frameborder="0"
   >
   </iframe>
