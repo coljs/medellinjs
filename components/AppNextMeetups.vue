6cf436202e3cb9f585a52be58a5f8f3bfa5a191f
Frank Betancur <frank.betancur@experimentality.co>
2023-12-29 14:42:44 -0500

    chore: sponsors deleted and fix heroes pictures

--- components/AppNextMeetups.vue
+++ components/AppNextMeetups.vue
@@ -1,162 +1,3 @@
-<style scoped>
-.meetup-tables {
-  float: left;
-  width: 100%;
-  margin-bottom: -30px;
-}
-
-.meetup-table {
-  float: left;
-  width: 100%;
-  height: 100%;
-  margin-bottom: 30px;
-  position: relative;
-}
-
-.meetup-heading {
-  background: #fff;
-  border-radius: 4px;
-  transition: all 0.3s ease;
-  height: 100%;
-}
-
-.meetup-event {
-  font-weight: 800;
-  color: #1293d5;
-  background-color: rgba(0, 0, 0, 0.11);
-  margin: 0;
-  padding: 33px 20px;
-  height: 60%;
-}
-
-h2.meetup-event {
-  text-transform: none;
-  font-size: 2rem;
-  line-height: 1.2;
-  /* show an ellipsis (...) if the meetup title is too long */
-  overflow: hidden;
-  text-overflow: ellipsis;
-}
-
-.event-meetup {
-  font-size: 1.2rem;
-  font-family: 'Roboto', sans-serif;
-  color: #1293d5;
-  margin: 0;
-  padding: 19px 20px;
-  height: 25%;
-}
-
-.meetup-confirm {
-  font-size: 1.1rem;
-  color: #1293d5;
-  background-color: rgba(0, 0, 0, 0.11);
-  height: 10%;
-  padding: 0rem 0.2rem;
-  margin: 0rem;
-}
-
-.meetup-cycle {
-  display: inline-block;
-  vertical-align: baseline;
-  font-size: 1.5rem;
-  font-weight: 400;
-  text-transform: lowercase;
-}
-
-.ribbon-wrapper {
-  overflow: hidden;
-  position: absolute;
-  top: -2px;
-  left: -2px;
-  bottom: -2px;
-  right: -2px;
-}
-
-.ribbon-wrapper .ribbon {
-  position: absolute;
-  padding: 0px 0;
-  font-weight: bold;
-  font-size: 1.1rem;
-  color: #333;
-  text-align: center;
-  background-color: #ebb134;
-  color: #fff;
-  width: 100px;
-}
-
-.ribbon.top-left {
-  -webkit-transform: rotate(-45deg);
-  -moz-transform: rotate(-45deg);
-  -ms-transform: rotate(-45deg);
-  -o-transform: rotate(-45deg);
-  top: 15px;
-  left: -25px;
-}
-
-.ribbon.top-right {
-  top: 15px;
-  right: -25px;
-  transform: rotate(45deg);
-  -webkit-transform: rotate(45deg);
-}
-
-.ribbon.bottom-left {
-  bottom: 15px;
-  left: -25px;
-  transform: rotate(45deg);
-  -webkit-transform: rotate(45deg);
-}
-
-.ribbon.bottom-right {
-  right: -25px;
-  bottom: 15px;
-  transform: rotate(-45deg);
-  -webkit-transform: rotate(-45deg);
-}
-
-.ribbon.white {
-  background: #f0f0f0;
-  color: #555;
-}
-.ribbon.black {
-  background: #333;
-}
-.ribbon.grey {
-  background: #999;
-}
-.ribbon.blue {
-  background: #39d;
-}
-.ribbon.green {
-  background: #2c7;
-}
-.ribbon.turquoise {
-  background: #1b9;
-}
-.ribbon.purple {
-  background: #95b;
-}
-.ribbon.red {
-  background: #e43;
-}
-.ribbon.orange {
-  background: #e82;
-}
-.ribbon.yellow {
-  background: #ec0;
-}
-
-/* Show the meetups tables with the same height */
-@media screen and (min-width: 768px) {
-  .meetup-tables {
-    display: -webkit-flex;
-    display: -ms-flexbox;
-    display: flex;
-  }
-}
-</style>
-
 <template>
   <div id="next-meetups-area" class="section section-padding-extra section-bg">
     <div class="container">
@@ -333,3 +174,162 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.meetup-tables {
+  float: left;
+  width: 100%;
+  margin-bottom: -30px;
+}
+
+.meetup-table {
+  float: left;
+  width: 100%;
+  height: 100%;
+  margin-bottom: 30px;
+  position: relative;
+}
+
+.meetup-heading {
+  background: #fff;
+  border-radius: 4px;
+  transition: all 0.3s ease;
+  height: 100%;
+}
+
+.meetup-event {
+  font-weight: 800;
+  color: #1293d5;
+  background-color: rgba(0, 0, 0, 0.11);
+  margin: 0;
+  padding: 33px 20px;
+  height: 60%;
+}
+
+h2.meetup-event {
+  text-transform: none;
+  font-size: 2rem;
+  line-height: 1.2;
+  /* show an ellipsis (...) if the meetup title is too long */
+  overflow: hidden;
+  text-overflow: ellipsis;
+}
+
+.event-meetup {
+  font-size: 1.2rem;
+  font-family: 'Roboto', sans-serif;
+  color: #1293d5;
+  margin: 0;
+  padding: 19px 20px;
+  height: 25%;
+}
+
+.meetup-confirm {
+  font-size: 1.1rem;
+  color: #1293d5;
+  background-color: rgba(0, 0, 0, 0.11);
+  height: 10%;
+  padding: 0rem 0.2rem;
+  margin: 0rem;
+}
+
+.meetup-cycle {
+  display: inline-block;
+  vertical-align: baseline;
+  font-size: 1.5rem;
+  font-weight: 400;
+  text-transform: lowercase;
+}
+
+.ribbon-wrapper {
+  overflow: hidden;
+  position: absolute;
+  top: -2px;
+  left: -2px;
+  bottom: -2px;
+  right: -2px;
+}
+
+.ribbon-wrapper .ribbon {
+  position: absolute;
+  padding: 0px 0;
+  font-weight: bold;
+  font-size: 1.1rem;
+  color: #333;
+  text-align: center;
+  background-color: #ebb134;
+  color: #fff;
+  width: 100px;
+}
+
+.ribbon.top-left {
+  -webkit-transform: rotate(-45deg);
+  -moz-transform: rotate(-45deg);
+  -ms-transform: rotate(-45deg);
+  -o-transform: rotate(-45deg);
+  top: 15px;
+  left: -25px;
+}
+
+.ribbon.top-right {
+  top: 15px;
+  right: -25px;
+  transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
+}
+
+.ribbon.bottom-left {
+  bottom: 15px;
+  left: -25px;
+  transform: rotate(45deg);
+  -webkit-transform: rotate(45deg);
+}
+
+.ribbon.bottom-right {
+  right: -25px;
+  bottom: 15px;
+  transform: rotate(-45deg);
+  -webkit-transform: rotate(-45deg);
+}
+
+.ribbon.white {
+  background: #f0f0f0;
+  color: #555;
+}
+.ribbon.black {
+  background: #333;
+}
+.ribbon.grey {
+  background: #999;
+}
+.ribbon.blue {
+  background: #39d;
+}
+.ribbon.green {
+  background: #2c7;
+}
+.ribbon.turquoise {
+  background: #1b9;
+}
+.ribbon.purple {
+  background: #95b;
+}
+.ribbon.red {
+  background: #e43;
+}
+.ribbon.orange {
+  background: #e82;
+}
+.ribbon.yellow {
+  background: #ec0;
+}
+
+/* Show the meetups tables with the same height */
+@media screen and (min-width: 768px) {
+  .meetup-tables {
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+  }
+}
+</style>
