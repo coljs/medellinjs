094bf18f6e44c7a245399ad0c26fb2110908236e
khriztianmoreno <khriztianmoreno@gmail.com>
2024-02-09 08:00:34 -0500

    feat: format asset organizers

--- assets/data/organizadores.js
+++ assets/data/organizadores.js
@@ -1,47 +1,4 @@
 module.exports = {
-  mariaserna: {
-    name: 'Maria Serna',
-    bio: 'Software developer with +5 years in software construction, speaker and active participant of the communities. Co-organizer @medellinjs - @node_co',
-    twitter: 'mafesernaarbole',
-    github: 'mafesernaarboleda',
-    avatar: 'https://avatars3.githubusercontent.com/u/14205513?s=250',
-    website: 'https://mafe.dev/',
-  },
-  edsadr: {
-    name: 'Adrián Estrada',
-    bio: 'Developer in love with NodeJS, thirsty for new technologies, working at @NodeSource - organizer @MedellinJS and @JSConfCO',
-    twitter: 'edsadr',
-    github: 'edsadr',
-    avatar:
-      'https://secure.gravatar.com/userimage/28946174/8747a95cd541d80ffa63929a4856637d.jpg?size=200',
-    website: 'https://twitter.com/edsadr',
-  },
-  ramirezalex: {
-    name: 'Alex Ramírez',
-    bio: 'Software Developer & Blockchain Padawan. I like it better if JavaScript == true. CTO at @INplazainc and co-organizer of @MedellinJS',
-    twitter: 'RamirezAlex_',
-    github: 'RamirezAlex',
-    avatar: 'https://avatars2.githubusercontent.com/u/1482473?s=460&v=4',
-    website: 'https://twitter.com/RamirezAlex_',
-  },
-  khriztianmoreno: {
-    name: 'Cristian Moreno',
-    bio: 'FullStack Javascript Developer and Community builder. Javascript Lover 👨🏻‍💻♥️👨🏻‍🏫 Co-Organize of @MedellinJS @avanet',
-    twitter: 'khriztianmoreno',
-    github: 'khriztianmoreno',
-    avatar:
-      'https://secure.gravatar.com/avatar/267898824a5115781fad35abba0130d0?s=250',
-    website: 'https://khriztianmoreno.dev/',
-  },
-  jenymazo: {
-    name: 'Jeny Mazo',
-    bio: 'Developer in love with NodeJS, Co-organizer at @MedellinJS and Mentor at @Coderiseorg',
-    twitter: 'JenyMzo',
-    github: 'JenyMzo',
-    avatar:
-      'https://avatars3.githubusercontent.com/u/2567952?v=3&u=93af511467354af56abba97d969cccca6fdffbef&s=250',
-    website: 'https://twitter.com/JenyMzo',
-  },
   frankbetancur: {
     name: 'Frank Alejo Betancur',
     bio: 'Systems Engineer, FullStack Padawan and Community builder. Learning... Co-organizer at @MedellinJS and @Avanet',
@@ -49,6 +6,14 @@ module.exports = {
     github: 'KranK2Me',
     avatar: 'https://avatars2.githubusercontent.com/u/9942486?s=460&v=4',
     website: 'https://www.frankbetancur.dev/',
+  },
+  mariaserna: {
+    name: 'Maria Serna',
+    bio: 'Software developer with +5 years in software construction, speaker and active participant of the communities. Co-organizer @medellinjs - @node_co',
+    twitter: 'mafesernaarbole',
+    github: 'mafesernaarboleda',
+    avatar: 'https://avatars3.githubusercontent.com/u/14205513?s=250',
+    website: 'https://mafe.dev/',
   },
   ivandev: {
     name: 'Ivan Villa',
@@ -59,6 +24,22 @@ module.exports = {
       'https://avatars0.githubusercontent.com/u/9753149?s=400&u=8db2280d571197e34825cd4fc8052bee8f1200db&v=4',
     website: 'https://twitter.com/IVilla85',
   },
+  mariafrancodev: {
+    name: 'Maria Alejandra Franco',
+    bio: 'Software engineer with a passion for web application development 👩🏻‍💻, active participant in the communities. Co-organizer at @MedellinJS.',
+    twitter: 'mariafrancodev',
+    github: 'mariafrancodev',
+    avatar: 'https://avatars.githubusercontent.com/u/42823670?v=4',
+    website: 'https://twitter.com/mariafrancodev',
+  },
+  dianella: {
+    name: 'Dianella Restrepo Chaverra',
+    bio: 'Systems Engineer with a passion for technology 👩🏻‍💻, active participant in the communities. Co-organizer at @MedellinJS.',
+    twitter: 'chzdiane',
+    github: 'chzdiane',
+    avatar: 'https://avatars.githubusercontent.com/u/50089098?v=4',
+    website: 'https://twitter.com/chzdiane',
+  },
   alexosman: {
     name: 'Alexander Osman',
     bio: 'Software developer, Passionate about web applications 👨🏽‍💻, active participant of the communities. Co-organizer at @MedellinJS.',
@@ -67,20 +48,22 @@ module.exports = {
     avatar: 'https://avatars.githubusercontent.com/u/98366329?v=4',
     website: 'https://twitter.com/FredyAl85651665',
   },
-  mariafrancodev: {
-    name: 'Maria Alejandra Franco',
-    bio: 'Software engineer with a passion for web application development 👩🏻‍💻, active participant in the communities. Co-organizer at @MedellinJS.',
-    twitter: 'mariafrancodev',
-    github: 'mariafrancodev',
-    avatar: 'https://avatars.githubusercontent.com/u/42823670?v=4',
-    website: 'https://twitter.com/mariafrancodev',
-    },
-    dianella: {
-      name: 'Dianella Restrepo Chaverra',
-      bio: 'Systems Engineer with a passion for technology 👩🏻‍💻, active participant in the communities. Co-organizer at @MedellinJS.',
-      twitter: 'chzdiane',
-      github: 'chzdiane',
-      avatar: 'https://avatars.githubusercontent.com/u/50089098?v=4',
-      website: 'https://twitter.com/chzdiane',
-      },
+  edsadr: {
+    name: 'Adrián Estrada',
+    bio: 'Developer in love with NodeJS, thirsty for new technologies, working at @NodeSource - organizer @MedellinJS and @JSConfCO',
+    twitter: 'edsadr',
+    github: 'edsadr',
+    avatar:
+      'https://secure.gravatar.com/userimage/28946174/8747a95cd541d80ffa63929a4856637d.jpg?size=200',
+    website: 'https://twitter.com/edsadr',
+  },
+  khriztianmoreno: {
+    name: 'Cristian Moreno',
+    bio: 'FullStack Javascript Developer and Community builder. Javascript Lover 👨🏻‍💻♥️👨🏻‍🏫 Co-Organize of @MedellinJS @avanet',
+    twitter: 'khriztianmoreno',
+    github: 'khriztianmoreno',
+    avatar:
+      'https://secure.gravatar.com/avatar/267898824a5115781fad35abba0130d0?s=250',
+    website: 'https://khriztianmoreno.dev/',
+  },
 }
